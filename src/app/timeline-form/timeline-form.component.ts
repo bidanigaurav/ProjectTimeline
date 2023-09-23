2513eb926588aac89b5ba4aac9a1d052dcc12de9
bidanigaurav <gauravbidaniwork@gmail.com>
2023-09-24 01:38:49 +0530

    timeline component created

---
+++ src/app/timeline-form/timeline-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timeline-form',
+  templateUrl: './timeline-form.component.html',
+  styleUrls: ['./timeline-form.component.css']
+})
+export class TimelineFormComponent {
+
+}
