cc02671b5f420e80326a33528e81b027e9a9b84e
bidanigaurav <gauravbidaniwork@gmail.com>
2023-09-25 02:01:00 +0530

    Timeline form and routing

--- src/PrimeNg/prime-ng/prime-ng.module.ts
+++ src/PrimeNg/prime-ng/prime-ng.module.ts
@@ -3,15 +3,35 @@ import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { PaginatorModule } from 'primeng/paginator';
+import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CardModule } from 'primeng/card';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 
 @NgModule({
   declarations: [],
-  imports: [
+  exports: [
     CommonModule,
     TableModule,
     ProgressSpinnerModule,
-    PaginatorModule
+    PaginatorModule,
+    InputTextareaModule,
+    InputNumberModule,
+    MultiSelectModule,
+    KeyFilterModule,
+    ButtonModule,
+    InputTextModule,
+    DropdownModule,
+    InputTextModule,
+    CalendarModule,
+    CardModule
   ]
 })
 export class PrimeNgModule { }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TimelineGridComponent } from './timeline-grid/timeline-grid.component';
+import { TimelineFormComponent } from './timeline-form/timeline-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TimelineGridComponent},
+  {path: 'new-project', component: TimelineFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
